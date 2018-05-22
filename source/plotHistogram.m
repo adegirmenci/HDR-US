22e65888f42283535fbca414b8010d4578f60ce9
Alperen Degirmenci <alperen89@gmail.com>
2018-05-22 17:29:02 -0400

    Edit copyright

--- source/plotHistogram.m
+++ source/plotHistogram.m
@@ -4,20 +4,20 @@ function plotHistogram(image, title_, saveResults, workingDir)
 % Example:
 %   plotHistogram(stack(:,:,1,1), 'Power: -26.7 dB', saveResults);
 %
-% Copyright (C) 2016-2017, by Alperen Degirmenci
+% Copyright (C) 2016-2018, by Alperen Degirmenci
 % 
 % This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
+% it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
 % 
-% You should have received a copy of the GNU Leser General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 % Histogram edges
 edges = linspace(0,1,256);
