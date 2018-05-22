12b573e19cc153ef821a8ada2fed40b235257019
Alperen Degirmenci <alperen89@gmail.com>
2018-05-22 17:28:32 -0400

    Edit copyright

--- installHDRUS.m
+++ installHDRUS.m
@@ -1,7 +1,7 @@
 %
 %     HDR-US Installer
 %
-%     Copyright (C) 2016 - 2017 Alperen Degirmenci
+%     Copyright (C) 2016 - 2018 Alperen Degirmenci
 % 
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
@@ -10,11 +10,11 @@
 % 
 %     This program is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 %     GNU General Public License for more details.
 % 
 %     You should have received a copy of the GNU General Public License
-%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
 
 %% First, install the HDR Toolbox
